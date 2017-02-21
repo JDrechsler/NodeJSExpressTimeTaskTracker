7c2e56072eb191314a9da8a4e8a2eef3164d52e6
Johannes Drechsler <jonjohnsonwright@gmail.com>
2017-02-21 15:36:08 +0100

    resolve github sync issues

---
+++ client.js
@@ -0,0 +1,99 @@
+var socket = io()
+
+var vueVars = {
+    table: '#example',
+    status: 'works',
+    newTask: '',
+    foregroundApp: '',
+    dataSet: [
+
+    ]
+}
+
+var vueMethods = {
+    addNewTask() {
+
+        if (vueVars.newTask == '') {
+            return
+        }
+        vueVars.dataSet.push([vueVars.newTask, 0, 0, 0])
+        vueVars.newTask = ''
+    }
+}
+
+var vueComputed = {
+
+}
+
+var vueWatchers = {
+    dataSet() {
+        refreshTable()
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    new Vue({
+        el: '#app',
+        data: vueVars,
+        methods: vueMethods,
+        computed: vueComputed,
+        watch: vueWatchers
+    })
+
+    document.ondragover = document.ondrop = (ev) => {
+        ev.preventDefault();
+    }
+    document.body.ondrop = (ev) => {
+        ev.preventDefault();
+    }
+
+    $(vueVars.table).DataTable({
+        data: vueVars.dataSet,
+        columns: [
+            { title: "Active window", "width": "50%" },
+            { title: "Time spent in seconds" },
+            { title: "Time spent in minutes" },
+            { title: "Time spent in hours" }
+        ]
+    })
+})
+
+socket.emit('halloVonClient')
+socket.on('halloVonServer', function (message) {
+    vueVars.status = 'works and connected to server'
+    console.log(message)
+})
+
+socket.on('response-foregroundApp', function (foregroundApp) {
+    vueVars.foregroundApp = foregroundApp
+
+    var taskExists = false
+
+    for (var i = 0; i < vueVars.dataSet.length; i++) {
+        //0-task
+        //1-timeSpent in s
+        //2-timeSpent in m
+        //3-timeSpent in h
+        var task = vueVars.dataSet[i][0] // task
+
+        if (task == foregroundApp) {
+            vueVars.dataSet[i][1] += 1 //seconds
+            vueVars.dataSet[i][2] = (vueVars.dataSet[i][1] / 60).toFixed(2) //minutes
+            vueVars.dataSet[i][3] = ((vueVars.dataSet[i][1] / 60) / 60).toFixed(2) //hours
+            taskExists = true
+            break
+        }
+    }
+    if (!taskExists) {
+        vueVars.dataSet.push([foregroundApp, 1, 0, 0])
+    }
+
+    refreshTable()
+})
+
+function refreshTable() {
+    var table = $(vueVars.table).DataTable()
+    table.clear()
+    table.rows.add(vueVars.dataSet)
+    table.draw()
+}
---
+++ server.js
@@ -0,0 +1,64 @@
+//TEMPLATE
+
+var express = require('express')
+var app = express()
+var server = require('http').Server(app)
+var port = process.env.PORT || 3000
+var io = require('socket.io')(server)
+var connectedIds = []
+var winprocess = require('./cpp/winprocess/build/Release/winprocess')
+
+server.listen(port, function () {
+    console.log(`Listening on Port: ${port}, https://web-entwicklung-anunymux.c9users.io`);
+    showConnectedClientIds()
+})
+
+app.use(express.static(__dirname))
+
+app.get('/', function (request, response) {
+    response.sendFile(__dirname + '/index.html')
+})
+
+
+io.on('connection', function (socket) {
+
+    connectedIds.push(socket.id)
+    console.log(`A client connected: ${socket.id}`)
+    showConnectedClientIds()
+
+    //The client sends 'getApi' event to server
+    socket.on('halloVonClient', function () {
+        socket.emit('halloVonServer', 'Server sagt hallo zu Client')
+    })
+
+    setInterval(function () {
+        socket.emit('response-foregroundApp', winprocess.getActiveWindowName())
+    }, 1000);
+
+    //A client disconnects from the server
+    socket.on('disconnect', function () {
+
+        for (var i = connectedIds.length - 1; i >= 0; i--) {
+            if (connectedIds[i] === socket.id) {
+                connectedIds.splice(i, 1)
+            }
+        }
+        console.log(`Client left: ${socket.id}`);
+        showConnectedClientIds()
+    })
+})
+
+showConnectedClientIds = function () {
+    console.log('');
+    console.log('Connected client ids:')
+
+    if (connectedIds.length < 1) {
+        console.log('none');
+    } else {
+        for (var i = 0; i < connectedIds.length; i++) {
+            console.log(connectedIds[i])
+        }
+    }
+
+    console.log('');
+}
